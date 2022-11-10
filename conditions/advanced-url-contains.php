3eb7e9546b3f9f254123f1240f9a5cb8bf131a48
Mykhailo Kulinich <57287929+ihslimn@users.noreply.github.com>
2022-11-10 17:50:25 +0200

    Update advanced-url-contains.php

--- conditions/advanced-url-contains.php
+++ conditions/advanced-url-contains.php
@@ -1,4 +1,6 @@
 <?php
+
+namespace Jet_Popup\Conditions;
 /**
  * Is front page condition
  */
@@ -9,89 +11,100 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 
+/**
+ * Define Jet_Popup_Conditions_Advanced_Url_Contains class
+ */
+class Jet_Popup_Conditions_Advanced_Url_Contains extends Base {
+
 	/**
-	 * Define Jet_Popup_Conditions_Advanced_Url_Contains class
+	 * Condition slug
+	 *
+	 * @return string
 	 */
-	class Jet_Popup_Conditions_Advanced_Url_Contains extends Jet_Popup_Conditions_Base {
+	public function get_id() {
+		return 'url-contains';
+	}
 
-		/**
-		 * Condition slug
-		 *
-		 * @return string
-		 */
-		public function get_id() {
-			return 'url-contains';
-		}
+	/**
+	 * Condition label
+	 *
+	 * @return string
+	 */
+	public function get_label() {
+		return __( 'URL Contains', 'jet-popup' );
+	}
 
-		/**
-		 * Condition label
-		 *
-		 * @return string
-		 */
-		public function get_label() {
-			return __( 'URL Contains', 'jet-popup' );
-		}
+	/**
+	 * Condition group
+	 *
+	 * @return string
+	 */
+	public function get_group() {
+		return 'advanced';
+	}
 
-		/**
-		 * Condition group
-		 *
-		 * @return string
-		 */
-		public function get_group() {
-			return 'advanced';
-		}
+	public  function get_priority() {
+		return 100;
+	}
 
-		/**
-		 * [get_control description]
-		 * @return [type] [description]
-		 */
-		public function get_control() {
-			return [
-				'type'        => 'input',
-				'placeholder' => __( 'Input string', 'jet-popup' ),
-			];
-		}
+	/**
+	 * @return string
+	 */
+	public function get_body_structure() {
+		return 'jet_page';
+	}
 
-		/**
-		 * [ajax_action description]
-		 * @return [type] [description]
-		 */
-		public function ajax_action() {
+	/**
+	 * [get_control description]
+	 * @return [type] [description]
+	 */
+	public function get_control() {
+		return [
+			'type'        => 'input',
+			'placeholder' => __( 'Input string', 'jet-popup' ),
+		];
+	}
+
+	/**
+	 * [ajax_action description]
+	 * @return [type] [description]
+	 */
+	public function ajax_action() {
+		return false;
+	}
+
+	/**
+	 * [get_label_by_value description]
+	 * @param  string $value [description]
+	 * @return [type]        [description]
+	 */
+	public function get_label_by_value( $value = '' ) {
+		return $value;
+	}
+
+	/**
+	 * Condition check callback
+	 *
+	 * @return bool
+	 */
+	public function check( $arg = '' ) {
+
+		if ( empty( $arg ) ) {
 			return false;
 		}
 
-		/**
-		 * [get_label_by_value description]
-		 * @param  string $value [description]
-		 * @return [type]        [description]
-		 */
-		public function get_label_by_value( $value = '' ) {
-			return $value;
+		$is_match = false;
+
+		global $wp;
+		$url = home_url( $wp->request );
+
+		if ( false !== strpos( $url, $arg ) ) {
+			$is_match = true;
 		}
 
-		/**
-		 * Condition check callback
-		 *
-		 * @return bool
-		 */
-		public function check( $arg = '' ) {
-
-			if ( empty( $arg ) ) {
-				return false;
-			}
-
-			$is_match = false;
-
-			global $wp;
-			$url = home_url( $wp->request );
-
-			if ( false !== strpos( $url, $arg ) ) {
-				$is_match = true;
-			}
-
-			return $is_match;
-		}
-
+		return $is_match;
 	}
+
+}
 
 
